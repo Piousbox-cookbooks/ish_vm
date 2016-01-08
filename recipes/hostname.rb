2d46ac5e9f02296f0083db7429e2eb666721b7d6
Victor Piousbox <piousbox@gmail.com>
2016-01-08 13:01:00 -0800

    correct chef spec

--- recipes/hostname.rb
+++ recipes/hostname.rb
@@ -1,5 +1,32 @@
 
-# search sites, get the hostname that belongs to me.
-
+#
+# recipe ish_vm::hostname
+# 20160108
+#
 # set the hostname
+#
+# node['network']['interfaces'].each { |k, v| v['addresses'].each { |kk, vv| puts kk }  }; 0
+#
+#
 
+addresses = []
+node['network']['interfaces'].each do |if_name, if_hash|
+  if_hash['addresses'].each do |address, address_hash|
+    if 'inet' == address_hash['family']
+      addresses.push address
+    end
+  end
+end
+
+hostname = nil
+sites = data_bag_item('utils', 'sites')['sites']
+sites.each do |site| # search sites, get the hostname that belongs to me.
+  if addresses.include? site['ipaddress']
+    hostname = site['domains'][0]
+  end
+end
+
+file "/etc/hostname" do
+  content hostname
+  only_if { hostname }
+end
--- spec/recipes/hostname_spec.rb
+++ spec/recipes/hostname_spec.rb
@@ -5,21 +5,38 @@ describe "ish_vm::hostname" do
 
   before :each do
     sites_data_bag = {
+      'sites' => [
+        {
+          'id' => 'some-id',
+          'domains' => [ 'localhost-special' ],
+          'ipaddress' => '127.0.0.1'
+        },
+        {}
+      ]
     }
-    stub_data_bag_item("utils", "sites").and_return( stubbed_data_bag )
+    stub_data_bag_item("utils", "sites").and_return( sites_data_bag )
     # stub_command("/usr/sbin/apache2 -t").and_return(true)
     # stub_command("mysql -usome-username -p -h  -se'USE ;' 2>&1").and_return(true)
   end
   
   let(:chef_run) do
     ChefSpec::SoloRunner.new do |node|
-      node.set['chef_environment'] = '_default'
-      # node.set['cookbook']['attribute'] = 'hello'
+      node.set['network'] = {
+        'interfaces' => {
+          'eth0' => {
+            'addresses' => {
+              '127.0.0.1' => {
+                'family' => 'inet'
+              }
+            }
+          }
+        }
+      }
     end.converge(described_recipe)
   end
 
   it 'sets hostname' do
-    expect(chef_run).to include_recipe("ish_apache::install_apache")
+    expect(chef_run).to create_file( "/etc/hostname" ).with( :content => 'localhost-special' )
   end
     
 end
